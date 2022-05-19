f38021ef5ad61b0e640d66ee339ac3cbdeef3532
Andrea Madsen <almadsen792@live.ca>
2022-05-18 21:46:44 -0600

    add and leverage formatSpots helper function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -11,11 +11,23 @@ export default function DayListItem(props) {
     "day-list__item--full": spots === 0
   }
   const dayClass = classNames(classConfig);
+
+  function formatSpots(num) {
+    if (num === 0) {
+      return 'no spots';
+    }
+
+    if (num === 1) {
+      return '1 spot';
+    } 
+
+    return `${num} spots`;
+  }
   
   return (
     <li className={dayClass} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2> 
-      <h3 className="text--light">{spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(spots)} remaining</h3>
     </li>
   );
 }
