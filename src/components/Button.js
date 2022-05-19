a675994ff0935f454d8b42e9eb45f54d422115ae
Andrea Madsen <almadsen792@live.ca>
2022-05-18 19:52:36 -0600

    add conditional styling, onClick, and disabled props

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,22 @@ import "components/Button.scss";
 
 export default function Button(props) {
    const label = props.children;
+   const { onClick, disabled } = props;
+
+   let buttonClass = "button";
+
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+   }
+
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
+
    return (
-      <button>{label}</button>
+      <button 
+         className={buttonClass}
+         onClick={onClick}
+         disabled={disabled}>{label}</button>
    );
 }
