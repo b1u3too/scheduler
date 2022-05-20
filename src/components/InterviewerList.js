7101c3593037d820c197b6fa8eeee3314d49211f
Andrea Madsen <almadsen792@live.ca>
2022-05-19 21:31:54 -0600

    add render helpers and place into return value

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,13 +3,31 @@ import InterviewerListItem from './InterviewerListItem';
 
 import "components/InterviewerList.scss";
 
-export default function InterviewerListItem (props) {
+export default function InterviewerList (props) {
   const { interviewers, interviewer, setInterviewer } = props;
+
+  function isSelected(idNum){
+    return idNum === interviewer;
+  }
+
+  const renderedInterviewers = interviewers.map(single => {
+    return (
+      <InterviewerListItem 
+        id={single.id}
+        name={single.name}
+        avatar={single.avatar}
+        selected={isSelected(single.id)}
+        setInterviewer={setInterviewer}
+      />
+    );
+  })
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {renderedInterviewers}
+      </ul>
     </section>
   );
 }
