dd9c1f0d750aeee1fbf0ece8a501fb0792efedd2
Andrea Madsen <almadsen792@live.ca>
2022-05-19 19:51:40 -0600

    implement DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import DayListItem from './DayListItem';
 
 export default function DayList(props) {
+  const { days, day, setDay } = props;
+
+  function isSelected(dayName) {
+    return day === dayName;
+  }
+
+  const renderedDays = days.map((oneDay) => {
+    return (
+      <DayListItem 
+        key={oneDay.id}
+        name={oneDay.name}
+        spots={oneDay.spots}
+        selected={isSelected(oneDay.name)}
+        setDay={setDay}
+      />
+    ); 
+  })
+
   return (
-    <ul></ul>
+    <ul>
+      {renderedDays}
+    </ul>
   );
 }
