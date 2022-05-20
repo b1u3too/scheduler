90ce4ba3707fa3295243fbfb507b6c89d0d004d5
Andrea Madsen <almadsen792@live.ca>
2022-05-19 20:36:12 -0600

    replace hard-coding with props

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import "components/InterviewerListItem.scss";
+
 export default function InterviewerListItem (props) {
+  const { id, name, avatar } = props;
+
   return (
     <li className="interviewers__item">
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={avatar}
+        alt={name}
       />
-      Sylvia Palmer
+      {name}
     </li>
   );
 }
