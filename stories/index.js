9222cbe4a3289a6c7f82b0e23e169b8ee9fab5eb
Andrea Madsen <almadsen792@live.ca>
2022-05-21 16:03:01 -0600

    initialize stories for Status component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 // ------------------------------------------------------------
 
@@ -170,3 +171,4 @@ storiesOf("Appointment", module)
     onConfirm={action("onConfirm")}
     onCancel={action("onCancel")}
     />)
+  .add("Status", () => <Status />)
