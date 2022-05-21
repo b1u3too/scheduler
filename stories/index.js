d26d341975109f318b680005cde7999d7b0da23e
Andrea Madsen <almadsen792@live.ca>
2022-05-21 15:58:12 -0600

    Add props to Confirm component and story

--- stories/index.js
+++ stories/index.js
@@ -14,7 +14,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
-import Button from "components/Appointment/Confirm";
+import Confirm from "components/Appointment/Confirm";
 
 // ------------------------------------------------------------
 
@@ -165,4 +165,8 @@ storiesOf("Appointment", module)
     onEdit={action("onEdit")}
     onDelete={action("onDelete")}
     />)
-  .add("Confirm", () => <Confirm />)
+  .add("Confirm", () => <Confirm 
+    message="Delete the appointment?"
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+    />)
