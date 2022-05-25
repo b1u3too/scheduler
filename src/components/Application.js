4726e5d0497b3e36688e09aa47beb465676a1929
Andrea Madsen <almadsen792@live.ca>
2022-05-24 22:00:05 -0600

    import Appointment component and render sample data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
 import DayList from "components/DayList";
@@ -21,6 +22,55 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+function renderAppointments(aptObject) {
+  
+  return Object.values(aptObject).map((appointment) => {
+    return (
+      <Appointment
+      key={appointment.id}
+      {...appointment} />
+    );
+  });
+}
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
@@ -48,7 +98,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {renderAppointments(appointments)}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
