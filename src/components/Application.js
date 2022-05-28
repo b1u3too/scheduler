0d289e9e54e5b260c733535713bff54d342e75e6
Andrea Madsen <almadsen792@live.ca>
2022-05-27 21:44:28 -0600

    install axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import Appointment from "./Appointment";
 
 import "components/Application.scss";
