3cf25118357b84a555ec4f7398f28bd1c7da1f74
Andrea Madsen <almadsen792@live.ca>
2022-05-30 16:52:19 -0600

    refactor: leverage useVisualMode custom hook for Show/Empty display

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,22 +1,28 @@
+//Utilities
 import React from 'react';
-
-import "./styles.scss";
-
+//Components
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+//helpers and hooks
+import useVisualMode from 'hooks/useVisualMode';
+//style
+import "./styles.scss";
 
 export default function Appointment (props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
   const { time, interview } = props;
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment">
       <Header time={time} />
-      { interview 
-        ? <Show 
-          student={interview.student}
-          interviewer={interview.interviewer}/> 
-        : <Empty />}
+      { mode === SHOW && <Show 
+        student={interview.student}
+        interviewer={interview.interviewer} /> }
+      { mode === EMPTY && <Empty /> }
     </article>
   );
 }
