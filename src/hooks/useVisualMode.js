0fd4cd24f5d527752093a0f5a2c6d11ce986d572
Andrea Madsen <almadsen792@live.ca>
2022-05-29 22:49:11 -0600

    refactor transition to allow replace option

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,16 +4,18 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  function transition(newMode) {
-    console.log("History, ", history);
-    setHistory([...history, newMode]);
+  function transition(newMode, replace = false) {
+    replace ? 
+      setHistory([...history.slice(0, -1), newMode]) :
+      setHistory([...history, newMode]);
+
     setMode(newMode);
   }
   function back() {
     if (history.length === 1) {
       return;
     }
-    
+
     const newHistory = history.slice(0, -1);
     setHistory(newHistory);
     setMode(newHistory[newHistory.length - 1]);
