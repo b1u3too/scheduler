54412475eac8a3a32fc38eabc5b670d707a8e49c
Andrea Madsen <almadsen792@live.ca>
2022-05-30 17:11:23 -0600

    feature: Add logic to allow users to open form and cancel out of form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 //helpers and hooks
 import useVisualMode from 'hooks/useVisualMode';
 //style
@@ -12,6 +13,7 @@ import "./styles.scss";
 export default function Appointment (props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = "CREATE";
 
   const { time, interview } = props;
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
@@ -21,8 +23,15 @@ export default function Appointment (props) {
       <Header time={time} />
       { mode === SHOW && <Show 
         student={interview.student}
-        interviewer={interview.interviewer} /> }
-      { mode === EMPTY && <Empty /> }
+        interviewer={interview.interviewer} 
+        /> }
+      { mode === EMPTY && <Empty 
+        onAdd={() =>transition(CREATE)}
+        /> }
+      { mode === CREATE && <Form 
+        interviewers={[]}
+        onCancel={back}
+        />}
     </article>
   );
 }
