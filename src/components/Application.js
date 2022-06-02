9a03432c0246a3702a04a81d41a9d00999cff4c5
Andrea Madsen <almadsen792@live.ca>
2022-06-01 21:19:36 -0600

    refactor bookInterview to update state

--- src/components/Application.js
+++ src/components/Application.js
@@ -20,7 +20,15 @@ export default function Application(props) {
   );
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: {...interview}
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    }
+    setState({ ...state, appointments });
   }
 
   const setDay = day => setState({...state, day});
