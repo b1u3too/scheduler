f3421fe97aff554ab2dbededa45b1f02e1cfed8e
Andrea Madsen <almadsen792@live.ca>
2022-06-12 16:45:19 -0600

    add: test for edit existing appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,19 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, waitForElementToBeRemoved, queryByText, queryByAltText } from "@testing-library/react";
+import {
+  cleanup,
+  fireEvent,
+  getAllByTestId,
+  getByAltText,
+  getByDisplayValue,
+  getByPlaceholderText,
+  getByText,
+  queryByAltText,
+  queryByText,
+  render,
+  waitForElement,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -70,6 +83,42 @@ describe("Application", () => {
     const days = getAllByTestId(container, "day");
     const day = days.find(data => queryByText(data, "Monday"));
     expect(getByText(day, /2 spots remaining/i)).toBeInTheDocument();
+  });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    //wait until page fully renders
+    const { container, debug } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    //get a non-empty appointment and click Edit
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments.find(data => queryByText(data, "Archie Cohen"));
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    //check that the form is pre-populated with name and interviewer
+    expect(getByDisplayValue(appointment, "Archie Cohen")).toBeInTheDocument();
+    expect(getByText(appointment, "Tori Malcolm")).toBeInTheDocument();
+
+    //change the name and interviewer and save
+    fireEvent.change(getByDisplayValue(appointment, "Archie Cohen"), {
+      target: { value: "Ursula K. LeGuin" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+    fireEvent.click(getByText(appointment, /save/i));
+
+    //check that saving dial appears, wait until it goes away to move on
+    expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => getByText(appointment, /saving/i));
+
+    //check that name and interviewer are updated
+    expect(getByText(appointment, "Ursula K. LeGuin")).toBeInTheDocument();
+    expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
+
+    //check that counter has stayed the same
+    const days = getAllByTestId(container, "day");
+    const day = days.find(data => queryByText(data, "Monday"));
+    expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
   });
 });
 
