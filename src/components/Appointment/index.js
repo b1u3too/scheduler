4668a6b8098cc2b4fa5f02d1ba5211c7e1f2ba83
Andrea Madsen <almadsen792@live.ca>
2022-06-12 14:10:22 -0600

    add data-testid attribute to appointment root

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -61,7 +61,10 @@ export default function Appointment (props) {
   }
 
   return (
-    <article className="appointment">
+    <article 
+      className="appointment"
+      data-testid="appointment"
+    >
       <Header time={time} />
       { 
         mode === SHOW && 
